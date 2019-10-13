b8c3dd274f3ccbccb77d0862d66820889bc55653
Jason Brown <jason.brown91@outlook.com>
2019-10-13 13:33:02 -0600

    Added image and product model classes

---
+++ src/main/java/com/skatemerch/model/Image.java
@@ -0,0 +1,29 @@
+package com.skatemerch.model;
+
+import java.util.UUID;
+
+public class Image {
+
+    private UUID id;
+    private UUID product_id;
+    private String url_to_image;
+
+    public Image(UUID product_id, String url_to_image) {
+        this.id = UUID.randomUUID();
+        this.product_id = product_id;
+        this.url_to_image = url_to_image;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getProduct_id() {
+        return product_id;
+    }
+
+    public String getUrl_to_image() {
+        return url_to_image;
+    }
+
+}
---
+++ src/main/java/com/skatemerch/model/Product.java
@@ -0,0 +1,39 @@
+package com.skatemerch.model;
+
+import java.util.UUID;
+
+public class Product {
+    private UUID id;
+    private String type;
+    private String link;
+    private UUID skater_id;
+
+    public Product(String type, String link, UUID skater_id) {
+        this.id = UUID.randomUUID();
+        this.type = type;
+        this.link = link;
+        this.skater_id = skater_id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public UUID getSkater_id() {
+        return skater_id;
+    }
+
+
+
+
+
+
+}
